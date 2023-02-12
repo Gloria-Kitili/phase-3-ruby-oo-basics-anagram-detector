2dcec5188aaa5c0de39555a8b7b4f98be1aae30c
root <gloria.kitili@student.moringaschool.com>
2023-02-12 12:51:35 +0300

    basics-anagram-detector

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def intialize (word)
+        @word = word
+    end
+
+    def match (array)
+     letter.sort === word.sort
+    end
+end
+
+char = Anagram.new ("re-arrange")

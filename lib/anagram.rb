46fd01efb0ed2f76c54f9e1bfee3aa1b838120c7
root <gloria.kitili@student.moringaschool.com>
2023-05-18 22:16:14 +0300

    anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,18 +3,18 @@
 class Anagram
     def initialize(word)
         @word = word.downcase
-        @sorted_word = @word.char.sort.join
+        @sorted_word = @word.chars.sort.join
     end
 
     def match(words)
-        word.select do |w|
-     w.downcase !=@word && w.downcase.chars.sort.join == @sorted_word
+        words.select do |w|
+     w.downcase !=@words && w.downcase.chars.sort.join == @sorted_word
         end 
     end
 end
 
 anagram = Anagram.new("final")
-anagram.match(["fnial","fianl", "lfian"])
+anagram.match(["fnial","fianl"])
 
 
   #attr_reader :word :match
